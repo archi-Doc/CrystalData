afc1b5dd1bbde8473ac48fed628a95758f5ebf29
archi-Doc <archidoc422@gmail.com>
2025-07-21 15:08:44 +0900

    .

--- CrystalData/Core/Storage/StoragePoint.cs
+++ CrystalData/Core/Storage/StoragePoint.cs
@@ -5,11 +5,40 @@ using Tinyhand.IO;
 
 namespace CrystalData;
 
+/// <summary>
+/// Represents the state of a <see cref="StoragePoint{TData}"/>.
+/// </summary>
 public enum StoragePointState
 {
+    /// <summary>
+    /// The function of <see cref="StoragePoint{TData}" /> is invalid, and the process is bypassed as is.
+    /// </summary>
+    NoStorage,
+
+    /// <summary>
+    /// The data is stored on storage and is not loaded into memory.
+    /// </summary>
     OnStorage,
+
+    /// <summary>
+    /// The data is loaded in memory.
+    /// </summary>
     InMemory,
+
+    /// <summary>
+    /// The data is loaded in memory and exclusively locked.
+    /// </summary>
     Locked,
+
+    /// <summary>
+    /// The unloading process is in progress, and operations on the data are disabled.
+    /// </summary>
+    UnloadingInProgress,
+
+    /// <summary>
+    /// The data is unloaded, and operations on the data are disabled.
+    /// </summary>
+    Unloaded,
 }
 
 /// <summary>
