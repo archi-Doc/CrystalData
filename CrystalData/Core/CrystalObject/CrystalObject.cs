10921fb441d03a78e4539f02360f22ad20651270
archi-Doc <archidoc422@gmail.com>
2025-09-09 16:59:02 +0900

    .

--- CrystalData/Core/CrystalObject/CrystalObject.cs
+++ CrystalData/Core/CrystalObject/CrystalObject.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using CrystalData.Filer;
-using CrystalData.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using Tinyhand.IO;
 
