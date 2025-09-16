01cc7e062c77c198947b6ec4fdce0cfa27bffc9b
archi-Doc <archidoc422@gmail.com>
2025-09-16 10:49:27 +0900

    .

---
+++ xUnitTest/Tests/StoragePointTest3.cs
@@ -0,0 +1,118 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System.Threading.Tasks;
+using CrystalData;
+using Tinyhand;
+using ValueLink;
+using Xunit;
+
+namespace xUnitTest.CrystalDataTest;
+
+#pragma warning disable SA1401 // Fields should be private
+
+[TinyhandObject(Structual = true)]
+public partial class SptClass
+{
+    public SptClass()
+    {
+    }
+
+    [Key(0)]
+    public int Id { get; set; }
+
+    [Key(1)]
+    public string Name = string.Empty;
+
+    [Key(2)]
+    public SptPoint.GoshujinClass SptGoshujin = new();
+
+    [Key(3)]
+    public StoragePoint<SptPoint.GoshujinClass> SptStorage { get; set; } = new();
+
+    public void SetIdAndName(int id, string name)
+    {
+        this.Id = id;
+        this.Name = name;
+    }
+
+    public void CheckIdAndName(int id, string name)
+    {
+        this.Id.Is(id);
+        this.Name.Is(name);
+    }
+
+    public async Task<SptClass> Add(int id)
+    {
+        using (var dataScope = await this.SptGoshujin.TryLock(id, AcquisitionMode.GetOrCreate))
+        {
+            if (dataScope.IsValid)
+            {
+                return dataScope.Data;
+            }
+            else
+            {
+                throw new Exception();
+                // return default;
+            }
+        }
+    }
+
+    public async Task<SptClass> Add2(int id)
+    {
+        using (var dataScope = await this.SptStorage.TryLock(id, AcquisitionMode.GetOrCreate))
+        {
+            if (dataScope.IsValid)
+            {
+                return dataScope.Data;
+            }
+            else
+            {
+                throw new Exception();
+                // return default;
+            }
+        }
+    }
+}
+
+[TinyhandObject(Structual = true)]
+[ValueLinkObject(Isolation = IsolationLevel.ReadCommitted)]
+public partial class SptPoint : StoragePoint<SptClass>
+{
+    [Key(1)]
+    [Link(Unique = true, Primary = true, Type = ChainType.Unordered)]
+    public int Id { get; set; }
+}
+
+public class StoragePointTest3
+{
+    [Fact]
+    public async Task Test1()
+    {
+        var crystal = await TestHelper.CreateAndStartCrystal<SptClass>(true);
+        var c1 = crystal.Data;
+
+        await this.Setup(c1);
+        await this.Check(c1);
+
+        await crystal.Store(StoreMode.ForceRelease);
+        await crystal.Crystalizer.StoreJournal();
+
+        await TestHelper.StoreAndReleaseAndDelete(crystal);
+    }
+
+    private async Task Setup(SptClass c1)
+    {
+        c1.SetIdAndName(1, "Root");
+        var c2 = await c1.Add(2);
+        c2.CheckIdAndName(2, string.Empty);
+    }
+
+    private async Task Check(SptClass c1)
+    {
+        c1.CheckIdAndName(1, "Root");
+        c1.SptGoshujin.Count.Is(1);
+        var array = c1.SptGoshujin.GetArray();
+        array.Length.Is(1);
+        (await array[0].TryGet()).CheckIdAndName(2, string.Empty);
+    }
+}
