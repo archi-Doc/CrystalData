2624cfee2bc953bd2751f93b99e4d8e2ec391f18
archi-Doc <archidoc422@gmail.com>
2026-04-04 10:16:26 +0900

    .

--- CrystalData/Core/StoragePoint/StorageObjectState.cs
+++ CrystalData/Core/StoragePoint/StorageObjectState.cs
@@ -2,8 +2,20 @@
 
 namespace CrystalData.Internal;
 
+/// <summary>
+/// Represents a set of state flags associated with a storage object.
+/// </summary>
 [Flags]
 internal enum StorageObjectState : byte
 {
+    /// <summary>
+    /// Indicates the object is pinned and guaranteed to remain only in memory<br/> and will never be released and written to disk.
+    /// </summary>
     Pinned = 1,
+
+    /// <summary>
+    /// The data has been invalidated and can no longer be used.<br/>
+    /// Use this before deleting the parent or for other similar purposes.
+    /// </summary>
+    Invalidated = 2,
 }
