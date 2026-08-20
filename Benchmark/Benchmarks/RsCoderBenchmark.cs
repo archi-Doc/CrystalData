0c600f8c9bd69feb5aa7398261a50229ffc908dc
archi-Doc <archidoc422@gmail.com>
2026-08-21 01:37:02 +0900

    .

--- Benchmark/Benchmarks/RsCoderBenchmark.cs
+++ Benchmark/Benchmarks/RsCoderBenchmark.cs
@@ -15,10 +15,15 @@ public class RsCoderBenchmark
     private const int ParityCount = 4;
     private const int Length = 12 * 1024 * 84;
 
+    private const int DataCount2 = 32;
+    private const int ParityCount2 = 16;
+
     private readonly RsCoder coder;
+    private readonly RsCoder coder2;
     private readonly byte[] source;
     private readonly byte[] destination;
     private readonly bool[] available;
+    private readonly bool[] available2;
 
     public RsCoderBenchmark()
     {
@@ -32,6 +37,28 @@ public class RsCoderBenchmark
 
         this.available = [true, false, true, false, false, true, true, true, true, true, true, true,];
         this.coder = new RsCoder(DataCount, ParityCount);
+        this.coder2 = new RsCoder(DataCount2, ParityCount2);
+
+        this.available2 = new bool[DataCount2 + ParityCount2];
+        var indices = new int[this.available2.Length];
+
+        for (var i = 0; i < indices.Length; i++)
+        {
+            indices[i] = i;
+        }
+
+        // Fisher-Yates shuffle.
+        for (var i = indices.Length - 1; i > 0; i--)
+        {
+            var j = Random.Shared.Next(i + 1);
+            (indices[i], indices[j]) = (indices[j], indices[i]);
+        }
+
+        // Randomly select exactly DataCount2 elements.
+        for (var i = 0; i < DataCount2; i++)
+        {
+            this.available2[indices[i]] = true;
+        }
     }
 
     [GlobalSetup]
@@ -75,6 +102,15 @@ public class RsCoderBenchmark
     {
         this.coder.Encode(this.source, this.destination);
         this.coder.Decode(this.destination, this.available, this.source.Length, this.source);
+
+        return this.source[0];
+    }
+
+    [Benchmark]
+    public int Test4()
+    {
+        this.coder2.Encode(this.source, this.destination);
+        this.coder2.Decode(this.destination, this.available2, this.source.Length, this.source);
 
         return this.source[0];
     }
--- xUnitTest/Tests/RsCoderTest.cs
+++ xUnitTest/Tests/RsCoderTest.cs
@@ -1,123 +1,61 @@
-// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
 
-using System.Numerics;
-using CrystalData;
+using System;
 using Xunit;
 
-#pragma warning disable SA1202 // Elements should be ordered by access
-
-namespace xUnitTest;
+namespace XunitTest;
 
 public class RsCoderTest
 {
-    [Fact]
-    public void ComprehensiveTest()
+    [Theory]
+    /*[InlineData(2, 1)]
+    [InlineData(3, 2)]
+    [InlineData(4, 2)]
+    [InlineData(8, 4)]
+    [InlineData(10, 5)]
+    [InlineData(16, 8)]*/
+    [InlineData(128, 64)]
+    public void EncodeDecodeRandomDataAndShards(int n, int m)
     {
-        // (int Data, int Check)[] nm = new[] { (4, 2), (4, 4), (4, 8), (8, 4), (8, 8), (8, 10), (16, 8), (16, 16), (5, 3), (5, 5), (13, 3), (13, 7), }; // Not supported
-        (int Data, int Check)[] nm = [(4, 2), (4, 4), (8, 2), (8, 4), (16, 2), (16, 4),];
+        const int Iterations = 1_000;
 
-        foreach (var x in nm)
-        {
-            this.ComprehensiveTestNM(x.Data, x.Check);
-            this.ComprehensiveTestNM_Data(x.Data, x.Check);
-        }
-    }
+        var random = new Random(12345);
+        var coder = new CrystalData.RsCoder(n, m);
 
-    private void ComprehensiveTestNM_Data(int n, int m)
-    {
-        var source = new byte[n];
-        for (var i = 0; i < n; i++)
+        var shardAvailable = new bool[n + m];
+        var indices = new int[n + m];
+
+        for (var i = 0; i < indices.Length; i++)
         {
-            source[i] = (byte)i;
+            indices[i] = i;
         }
 
-        using (var coder = new RsCoderObsolete(n, m))
+        for (var iteration = 0; iteration < Iterations; iteration++)
         {
-            var total = 1 << coder.TotalSize;
-            for (uint i = 0; i < total; i++)
+            var sourceLength = random.Next(1, 128);
+            var source = new byte[sourceLength];
+            random.NextBytes(source);
+
+            var encoded = coder.Encode(source);
+
+            // Shuffle shard indices.
+            for (var i = indices.Length - 1; i > 0; i--)
             {
-                if (BitOperations.PopCount(i) < n)
-                {// N blocks of valid data is required.
-                    continue;
-                }
-
-                coder.Encode(source, source.Length);
-                coder.InvalidateEncodedBufferForUnitTest(i);
-                coder.Decode(coder.EncodedBuffer!, coder.EncodedBufferLength);
-                TestHelper.ByteArrayEquals(source, coder.DecodedBuffer, source.Length).IsTrue();
+                var j = random.Next(i + 1);
+                (indices[i], indices[j]) = (indices[j], indices[i]);
             }
-        }
-    }
 
-    private void ComprehensiveTestNM(int n, int m)
-    {
-        using (var coder = new RsCoderObsolete(n, m))
-        {
-            var total = 1 << coder.TotalSize;
-            for (uint i = 0; i < total; i++)
+            Array.Clear(shardAvailable);
+
+            // Exactly N of N+M shards are available.
+            for (var i = 0; i < n; i++)
             {
-                if (BitOperations.PopCount(i) < n)
-                {// N blocks of valid data is required.
-                    continue;
-                }
-
-                coder.TestReverseMatrix(i);
+                shardAvailable[indices[i]] = true;
             }
-        }
-    }
 
-    [Fact]
-    public void RandomTest()
-    {
-        (int Data, int Check)[] nm = [(4, 2), (4, 4), (8, 2), (8, 4), (16, 2), (16, 4),];
-        var sizes = new[] { 0, 4, 16, 256, 1000, 10_000 };
+            var decoded = coder.Decode(encoded, shardAvailable, source.Length);
 
-        var random = new Random(42);
-        var sources = new byte[sizes.Length][];
-        for (var n = 0; n < sizes.Length; n++)
-        {
-            sources[n] = new byte[sizes[n]];
-            random.NextBytes(sources[n]);
-        }
-
-        foreach (var x in nm)
-        {
-            this.RandomTestNM(x.Data, x.Check, sources, random);
-        }
-    }
-
-    private void RandomTestNM(int n, int m, byte[][] sources, Random random)
-    {
-        // using (var coder = new RsCoder)
-
-        using (var coder = new RsCoderObsolete(n, m))
-        {
-            foreach (var x in sources)
-            {
-                this.RandomTestSource(coder, x, random);
-            }
-        }
-    }
-
-    private void RandomTestSource(RsCoderObsolete coder, byte[] source, Random random)
-    {
-        var length = source.Length;
-        length = (length / coder.DataSize) * coder.DataSize; // length must be a multiple of coder.DataSize
-
-        // Simple encode and decode.
-        coder.Encode(source, length);
-        coder.Decode(coder.EncodedBuffer!, coder.EncodedBufferLength);
-        TestHelper.ByteArrayEquals(source, coder.DecodedBuffer, length).IsTrue();
-
-        for (var i = 1; i <= coder.CheckSize; i++)
-        {
-            for (var j = 0; j < 10; j++)
-            {
-                coder.Encode(source, length);
-                coder.InvalidateEncodedBufferForUnitTest(random, i);
-                coder.Decode(coder.EncodedBuffer!, coder.EncodedBufferLength);
-                TestHelper.ByteArrayEquals(source, coder.DecodedBuffer, length).IsTrue();
-            }
+            Assert.Equal(source, decoded);
         }
     }
 }
